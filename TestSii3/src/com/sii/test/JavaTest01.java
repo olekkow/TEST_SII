7cfc035f39fade4e976f1472492c9130b1ce64db
olekkow <olekkow@wp.pl>
2023-01-05 10:34:19 +0100

    test

--- TestSii3/src/com/sii/test/JavaTest01.java
+++ TestSii3/src/com/sii/test/JavaTest01.java
@@ -9,9 +9,9 @@ public class JavaTest01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Map<String,Integer> mapa = new HashMap<String,Integer>();
-		mapa.put("jeden", new Integer(1));
-		mapa.put("dwa", new Integer(2));
-		mapa.put("trzy", new Integer(3));
+		mapa.put("jeden", 1);
+		mapa.put("dwa", 2);
+		mapa.put("trzy", 3);
 		
 		Iterator<String> it = mapa.keySet().iterator();
 		while(it.hasNext()) {
